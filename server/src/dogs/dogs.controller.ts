535b46ed4ae5a26944a69bf6f1b2e48f9904fd85
GrischK <grischka.gorski@laposte.net>
2025-01-11 19:58:12 +0100

    ✨ Add dogs service

---
+++ server/src/dogs/dogs.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { DogsService } from './dogs.service';
+import { Dog } from './dog.entity';
+
+@Controller('dogs')
+export class DogsController {
+    constructor(private readonly dogsService: DogsService) {}
+
+    @Get()
+    findAll(): Promise<Dog[]> {
+        return this.dogsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): Promise<Dog> {
+        return this.dogsService.findOne(+id);
+    }
+
+    @Post()
+    create(@Body() dog: Partial<Dog>): Promise<Dog> {
+        return this.dogsService.create(dog);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() dog: Partial<Dog>): Promise<Dog> {
+        return this.dogsService.update(+id, dog);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string): Promise<void> {
+        return this.dogsService.delete(+id);
+    }
+}
---
+++ server/src/dogs/dogs.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Dog } from './dog.entity';
+
+@Injectable()
+export class DogsService {
+    constructor(
+        @InjectRepository(Dog)
+        private readonly dogRepository: Repository<Dog>,
+    ) {}
+
+    findAll(): Promise<Dog[]> {
+        return this.dogRepository.find();
+    }
+
+    findOne(id: number): Promise<Dog> {
+        return this.dogRepository.findOne({ where: { id } });
+    }
+
+    create(dog: Partial<Dog>): Promise<Dog> {
+        const newDog = this.dogRepository.create(dog);
+        return this.dogRepository.save(newDog);
+    }
+
+    async update(id: number, updateData: Partial<Dog>): Promise<Dog> {
+        await this.dogRepository.update(id, updateData);
+        return this.dogRepository.findOne({ where: { id } });
+    }
+
+    async delete(id: number): Promise<void> {
+        await this.dogRepository.delete(id);
+    }
+}
